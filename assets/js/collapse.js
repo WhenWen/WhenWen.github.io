01a4aa9baa1e7d49decb77910ed2102a02a60702
WhenWen <kaiyuewen3@gmail.com>
2025-11-20 23:19:07 +0000

    Deploying to gh-pages from @ WhenWen/WhenWen.github.io@d2f9ea8f77a164252bd950c1028a1677cb89c2a6 🚀

---
+++ assets/js/collapse.js
@@ -0,0 +1,1 @@
+$(".header").click(function(){$header=$(this),$content=$header.next(),$content.slideToggle(500,function(){$header.text(function(){return $content.is(":visible")?"Collapse":"Expand"})})});
